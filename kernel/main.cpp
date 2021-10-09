bbb0b5e803c6dea45b14c8edced5e5bbd17978f8
tamagohan <fantastipo23@gmail.com>
2021-10-09 13:35:51 +0900

    [Day05e] Implement console

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -11,6 +11,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
+#include "console.hpp"
 
 void *operator new(size_t size, void *buf)
 {
@@ -45,25 +46,15 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  for (int x = 0; x < 200; ++x)
-  {
-    for (int y = 0; y < 100; ++y)
-    {
-      pixel_writer->Write(x, y, {255, 255, 0});
-    }
-  }
 
-  int i = 0;
-  for (char c = '!'; c <= '~'; ++c, ++i)
-  {
-    WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-  }
-  WriteString(*pixel_writer, 0, 66, "Hello, World! tamagohan", {0, 0, 255});
+  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
   char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
-
+  for (int i = 0; i < 23; ++i)
+  {
+    sprintf(buf, "line %d\n", i);
+    console.PutString(buf);
+  }
   while (1)
     __asm__("hlt");
 }
--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -1,5 +1,8 @@
+#include <errno.h>
 #include <sys/types.h>
 
-caddr_t sbrk(int incr) {
+caddr_t sbrk(int incr)
+{
+  errno = ENOMEM;
   return NULL;
 }
